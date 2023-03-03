6f550ce3e8c439f29c894e84b63fc899cb3232a9
rebecabergottini <rebeca._bergo@hotmail.com>
2023-03-03 20:07:27 +0000

    final

--- tours/src/App.js
+++ tours/src/App.js
@@ -7,9 +7,12 @@ const url = 'https://course-api.com/react-tours-project';
 function App() {
   
   const [loading, setLoading] = useState(true);
-  
   const [tours, setTours] = useState([]);
   
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id);
+    setTours(newTours);
+  }
   const fetchTours = async () => {
     setLoading(true)
 
@@ -34,10 +37,19 @@ function App() {
       </main>
     );
   }
-
+  if (tours.length === 0) {
+    return <main>
+      <div className='tittle'>
+        <h2>No tours left</h2>
+        <button className='btn' onClick={fetchTours}>
+          Refresh
+        </button>
+      </div>
+    </main>
+  }
   return (
     <main>
-    <Tours tours={tours}/>
+    <Tours tours={tours} removeTour={removeTour} />
     </main>
   );
 }
--- tours/src/componentes/Tours.js
+++ tours/src/componentes/Tours.js
@@ -1,8 +1,20 @@
 import React from "react";
 import Tour from "./Tour";
 
-const Tours = ({ tours }) => {
-  return 
-}
+const Tours = ({ tours, removeTour }) => {
+  return (
+    <section>
+      <div className="tittle">
+        <h2>Ours tours</h2>
+        <div className="underline"></div>
+      </div>
+      <div>
+        {tours.map((tour)=>{
+          return <Tour key={tour.id} {...tour} removeTour={removeTour}></Tour>
+        })}
+      </div>
+  </section>
+  );
+};
 
-export default Tours;s
+export default Tours;
